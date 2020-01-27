aaf89d264d8e4223f4d439d0d3a5870689310ba2
whoisabbie <44428844+whoisabbie@users.noreply.github.com>
2020-01-27 15:02:49 +0530

    Added files of To-Do-App v2.
    Added Autofocus for Directly adding to-do.
    Added query for checking if title of new to-do is empty then it won't be added.
    Solved issues of key(id), it'll take last id and increment it by one every-time & if no to-do is available then id will be started from 1.

---
+++ to-do-app-v2/build/precache-manifest.f0bf7d9a8089dc50ddcb82085823efe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3131ac337b1213d10d149cfa5c5104cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc5fa54a49a1c3b0dd80",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0c3a21ee342871902e0b",
+    "url": "/static/js/2.498cb688.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.498cb688.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc5fa54a49a1c3b0dd80",
+    "url": "/static/js/main.ee9fe03a.chunk.js"
+  },
+  {
+    "revision": "18b421c30108e3b3ce80",
+    "url": "/static/js/runtime-main.ff4a9e13.js"
+  }
+]);
